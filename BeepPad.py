fccdd84926b41f1359652d0928ead9a18495a5c8
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2021-07-13 14:09:25 +0700

    Update BeepPad.py

--- BeepPad.py
+++ BeepPad.py
@@ -6,7 +6,7 @@ from threading import Thread
 __author__ = "AkaraSellegg"
 __copyright__ = "Copyright 2019, BeepPad Project"
 __license__ = "MIT"
-__version__ = "0.1.0"
+__version__ = "0.2.1"
 __maintainer__ = "AkaraSellegg"
 __status__ = "Prototype"
 
